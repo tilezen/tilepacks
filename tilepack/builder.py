bd63da82849457c801af19d24b67fa183a5210d3
Tomas Vajda <tomas.vajda@vecturagames.com>
2017-06-27 00:37:07 +0200

    Adds support for downloading normal tiles. Adds support for deflating map tiles.

--- tilepack/builder.py
+++ tilepack/builder.py
@@ -10,17 +10,18 @@ import traceback
 import mercantile
 import logging
 import csv
+import gzip
 
 sess = requests.Session()
 adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=200)
 sess.mount('https://', adapter)
 
-# def fetch_tile(x, y, z, layer, format, api_key):
+# def fetch_tile(x, y, z, typ, layer, format, api_key):
 def fetch_tile(format_args):
     sleep_time = 0.5 * random.uniform(1.0, 1.7)
     response_info = []
     while True:
-        url = 'https://tile.mapzen.com/mapzen/vector/v1/{layer}/{zoom}/{x}/{y}.{fmt}?api_key={api_key}'.format(**format_args)
+        url = 'https://tile.mapzen.com/mapzen/{typ}/v1/512/{layer}/{zoom}/{x}/{y}.{fmt}?api_key={api_key}'.format(**format_args)
         try:
             start = time.time()
 
@@ -43,7 +44,7 @@ def fetch_tile(format_args):
         except requests.exceptions.RequestException as e:
             if isinstance(e, requests.exceptions.HTTPError):
                 if e.response.status_code == 404:
-                    print("HTTP 404 -- {} while retrieving {}. Not trying again.".format(
+                    print("HTTP 404 -- {} while retrieving {}. {} Not trying again.".format(
                         e.response.status_code, e.response.text, url)
                     )
                     return (format_args, response_info, None)
@@ -69,11 +70,11 @@ output_type_mapping = {
 }
 
 def build_tile_packages(min_lon, min_lat, max_lon, max_lat, min_zoom, max_zoom,
-        layer, tile_format, output, output_formats, api_key, concurrency):
+        typ, layer, tile_format, output, output_formats, api_key, concurrency):
 
     fetches = []
     for x, y, z in mercantile.tiles(min_lon, min_lat, max_lon, max_lat, range(min_zoom, max_zoom + 1)):
-        fetches.append(dict(x=x, y=y, zoom=z, layer=layer, fmt=tile_format, api_key=api_key))
+        fetches.append(dict(x=x, y=y, zoom=z, typ=typ, layer=layer, fmt=tile_format, api_key=api_key))
 
     tiles_to_get = len(fetches)
     tiles_written = 0
@@ -100,6 +101,8 @@ def build_tile_packages(min_lon, min_lat, max_lon, max_lat, min_zoom, max_zoom,
             t.add_metadata('name', output)
             # FIXME: Need to include the `json` key
             t.add_metadata('format', 'application/vnd.mapbox-vector-tile')
+            if typ == 'vector':
+                t.add_metadata('compression', 'deflate')
             t.add_metadata('bounds', ','.join(map(str, [min_lon, min_lat, max_lon, max_lat])))
             t.add_metadata('minzoom', min_zoom)
             t.add_metadata('maxzoom', max_zoom)
@@ -107,7 +110,10 @@ def build_tile_packages(min_lon, min_lat, max_lon, max_lat, min_zoom, max_zoom,
         for format_args, response_info, data in p.imap_unordered(fetch_tile, fetches):
             for t in tile_ouputters:
                 if data:
-                    t.add_tile(format_args, data)
+                    if typ == 'vector':
+                        t.add_tile(format_args, gzip.compress(data))
+                    else:
+                        t.add_tile(format_args, data)
 
             response_info_writer.writerows(response_info)
 
@@ -158,6 +164,10 @@ def main():
     parser.add_argument('max_zoom',
         type=int,
         help='The maximum zoom level to include')
+    parser.add_argument('typ',
+        help='Type - vector or terrain')
+    parser.add_argument('layer',
+        help='Layer')
     parser.add_argument('output',
         help='The filename for the output tile package')
     parser.add_argument('--tile-format',
@@ -182,7 +192,8 @@ def main():
         args.max_lat,
         args.min_zoom,
         args.max_zoom,
-        'all',
+        args.typ,
+        args.layer,
         args.tile_format,
         args.output,
         output_formats,
