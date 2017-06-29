dee8edfb451632029030bcb4a8281ddf7ac47579
Tomas Vajda <tomas.vajda@vecturagames.com>
2017-06-29 11:51:15 +0200

    Fixed according to review. Improved arguments.

--- tilepack/builder.py
+++ tilepack/builder.py
@@ -16,12 +16,12 @@ sess = requests.Session()
 adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=200)
 sess.mount('https://', adapter)
 
-# def fetch_tile(x, y, z, typ, layer, format, api_key):
+# def fetch_tile(x, y, z, type, layer, tile_size, tile_format, api_key):
 def fetch_tile(format_args):
     sleep_time = 0.5 * random.uniform(1.0, 1.7)
     response_info = []
     while True:
-        url = 'https://tile.mapzen.com/mapzen/{typ}/v1/512/{layer}/{zoom}/{x}/{y}.{fmt}?api_key={api_key}'.format(**format_args)
+        url = 'https://tile.mapzen.com/mapzen/{type}/v1/{size}/{layer}/{zoom}/{x}/{y}.{fmt}?api_key={api_key}'.format(**format_args)
         try:
             start = time.time()
 
@@ -44,12 +44,12 @@ def fetch_tile(format_args):
         except requests.exceptions.RequestException as e:
             if isinstance(e, requests.exceptions.HTTPError):
                 if e.response.status_code == 404:
-                    print("HTTP 404 -- {} while retrieving {}. {} Not trying again.".format(
+                    print("HTTP {} -- {} while retrieving {}. Not trying again.".format(
                         e.response.status_code, e.response.text, url)
                     )
                     return (format_args, response_info, None)
                 else:
-                    print("HTTP error {} -- {} while retrieving {}, retrying after {:0.2f} sec".format(
+                    print("HTTP {} -- {} while retrieving {}, retrying after {:0.2f} sec".format(
                         e.response.status_code, e.response.text, url, sleep_time)
                     )
             else:
@@ -70,11 +70,11 @@ output_type_mapping = {
 }
 
 def build_tile_packages(min_lon, min_lat, max_lon, max_lat, min_zoom, max_zoom,
-        typ, layer, tile_format, output, output_formats, api_key, concurrency):
+        type, layer, tile_size, tile_format, tile_compression, output, output_formats, api_key, concurrency):
 
     fetches = []
     for x, y, z in mercantile.tiles(min_lon, min_lat, max_lon, max_lat, range(min_zoom, max_zoom + 1)):
-        fetches.append(dict(x=x, y=y, zoom=z, typ=typ, layer=layer, fmt=tile_format, api_key=api_key))
+        fetches.append(dict(x=x, y=y, zoom=z, type=type, layer=layer, size=tile_size, fmt=tile_format, api_key=api_key))
 
     tiles_to_get = len(fetches)
     tiles_written = 0
@@ -101,7 +101,7 @@ def build_tile_packages(min_lon, min_lat, max_lon, max_lat, min_zoom, max_zoom,
             t.add_metadata('name', output)
             # FIXME: Need to include the `json` key
             t.add_metadata('format', 'application/vnd.mapbox-vector-tile')
-            if typ == 'vector':
+            if tile_compression:
                 t.add_metadata('compression', 'deflate')
             t.add_metadata('bounds', ','.join(map(str, [min_lon, min_lat, max_lon, max_lat])))
             t.add_metadata('minzoom', min_zoom)
@@ -110,7 +110,7 @@ def build_tile_packages(min_lon, min_lat, max_lon, max_lat, min_zoom, max_zoom,
         for format_args, response_info, data in p.imap_unordered(fetch_tile, fetches):
             for t in tile_ouputters:
                 if data:
-                    if typ == 'vector':
+                    if tile_compression:
                         t.add_tile(format_args, gzip.compress(data))
                     else:
                         t.add_tile(format_args, data)
@@ -164,17 +164,29 @@ def main():
     parser.add_argument('max_zoom',
         type=int,
         help='The maximum zoom level to include')
-    parser.add_argument('typ',
-        help='Type - vector or terrain')
-    parser.add_argument('layer',
-        help='Layer')
     parser.add_argument('output',
         help='The filename for the output tile package')
+    parser.add_argument('--layer',
+        default='all',
+        help='Layer')
+    parser.add_argument('--type',
+        default='vector',
+        choices=['vector', 'terrain'],
+        help='Type (vector or terrain)')
+    parser.add_argument('--tile-size',
+        default=512,
+        choices=[256, 512],
+        help='The size of tile (256 or 512)')
     parser.add_argument('--tile-format',
         default='mvt',
         help='The Mapzen Vector Tile format to request')
+    parser.add_argument('--tile-compression',
+        default=False,
+        action='store_true',
+        help='Compression method to compress individual tiles')
     parser.add_argument('--output-formats',
         default='mbtiles,zipfile',
+        choices=['mbtiles', 'zipfile', 'null'],
         help='A comma-separated list of output formats to write to (mbtiles, zipfile, or null)')
     parser.add_argument('-j', '--concurrency',
         type=int,
@@ -192,9 +204,11 @@ def main():
         args.max_lat,
         args.min_zoom,
         args.max_zoom,
-        args.typ,
+        args.type,
         args.layer,
+        args.tile_size,
         args.tile_format,
+        args.tile_compression,
         args.output,
         output_formats,
         api_key,
